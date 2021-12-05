c4645e269baf6f391ba4d4221a9e51e7f1b57a91
Bhaskar <karanbhaskar@Bhaskars-MacBook-Pro.local>
2021-12-05 02:19:42 -0500

    created axios get and post for index.html

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -23,6 +23,56 @@ const formEvent = form.addEventListener('submit', event => {
    
 });
 
+
+
+
+
+const addComments = (responseData) => {
+    const usersListEL = document.querySelector("#view__area");
+    usersListEL.innerHTML = ""
+
+   responseData.forEach(comment2 => {
+        const commentContent = document.createElement('div');
+        commentContent.classList.add('view__content');
+        usersListEL.appendChild(commentContent);
+
+        const thumbnail = document.createElement('img');
+        thumbnail.classList.add('view__thumbnail');
+        thumbnail.setAttribute('src', comment2.thumbnail);
+        commentContent.appendChild(thumbnail);
+
+        const name = document.createElement('p');
+        name.classList.add('view__name');
+        name.innerText = comment2.name;
+        commentContent.appendChild(name);
+
+
+        const date = document.createElement('p');
+        date.classList.add('view__date');
+        date.innerText = new Date(parseInt(comment2.timestamp));
+        commentContent.appendChild(date);
+
+        const remarks = document.createElement('p');
+        remarks.innerText = comment2.comment;
+        remarks.classList.add('view__remarks');
+        commentContent.appendChild(remarks);
+    });
+}
+
+const today = new Date();   
+const date1 =today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+console.log(date1);
+
+axios
+.get('https://project-1-api.herokuapp.com/comments?api_key=d67fc7e7-0b23-4412-b210-2a0744180a')
+.then(response => {
+    console.log(response.data);
+    addComments(response.data);
+    return response.data;
+})
+
+.catch((err) => console.log(err))
+
 const createUser = (user) => {
     axios.post('https://reqres.in/api/users', user)
         .then(response => {
@@ -36,97 +86,3 @@ const createUser = (user) => {
 
 
 
-
-
-// // const  BANDSITE_API_URL = () => `https://project-1-api.herokuapp.com/?api_key=api_key=${API_KEY}`;
-// // const API_KEY = "d67fc7e7-0b23-4412-b210-2a0744180a84";
-
-// // let comment_id = '';
-
-// const commenttext = [
-//     {   thumbnail: " ",
-//         name: "Connor Walton", 
-//         date: "02/17/2021", 
-//         remarks: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-//     },
-
-//     {
-//         thumbnail: " ",
-//         name: "Emilie Beach", 
-//         date: "01/09/2021", 
-//         remarks: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-//     },
-
-//     {
-//         thumbnail: " ",
-//         name: "Miles Acosta", 
-//         date: "12/20/2020", 
-//         remarks: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-//     }
-// ]
-
-// //comment content
-// function createCommentCard(comment) {
-//     const commentContent = document.createElement('div');
-//     commentContent.classList.add('view__content');
-
-//     const thumbnail = document.createElement('img');
-//     thumbnail.classList.add('view__thumbnail');
-//     thumbnail.setAttribute('src', comment.thumbnail);
-//     commentContent.appendChild(thumbnail);
-
-//     const name = document.createElement('p');
-//     name.classList.add('view__name');
-//     name.innerText = comment.name;
-//     commentContent.appendChild(name);
-
-
-//     const date = document.createElement('p');
-//     date.classList.add('view__date');
-//     date.innerText = comment.date;
-//     commentContent.appendChild(date);
-
-//     const remarks = document.createElement('p');
-//     remarks.innerText = comment.remarks;
-//     remarks.classList.add('view__remarks');
-//     commentContent.appendChild(remarks);
-
-//     return commentContent;
-
-// }
-
-// function renderComments() {
-//     const commentAreaEl = document.getElementById('view__area');
-//     commentAreaEl.innerHTML = "";
-    
-//     //render all comments
-//     for (let i = 0; i<commenttext.length; i++) {
-//         const card = createCommentCard(commenttext[i]);
-//         commentAreaEl.appendChild(card);
-//     }
-// }
-
-// const today = new Date();   
-// const date =today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
-// console.log(date);
-
-
-
-
-// function handleCommentSubmit (event) {
-//     event.preventDefault();
-
-//     const cardData = {
-//     name: event.target.fullName.value,
-//     date: date,
-//     remarks: event.target.commentsec.value
-//     };
-
-//     commenttext.unshift(cardData);
-//     renderComments();
-
-// }
-
-// const formEl = document.querySelector('.comment-form');
-// formEl.addEventListener('submit', handleCommentSubmit);
-// renderComments();
